8869b1e1a0b069e6c12b3815db7ae8528828fa6e
Martin <martin.tehvanus@gmail.com>
2025-12-28 15:24:20 +0200

    Small changes to main.py, testing while loop

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # imports and setup
+from unittest import TextTestResult
 import pygame
 import random
 import sys
 pygame.init()
 
 # some predefined variables
-
-
+line_color = "white"
+start_x_cord = 1
+height = 10
+test = 0
 
 # pygame window settings
 screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Sorting Algorithm")
-screen.fill("white")
+screen.fill("black")
 
 running = True
 
@@ -22,6 +25,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # program code below
+
+    while test != 100:
+        pygame.draw.rect(screen, line_color, pygame.Rect(start_x_cord, 1020, 10, 30))
+        start_x_cord += 15
+        test+=1
 
     # dont change what is below this
     pygame.display.flip()
